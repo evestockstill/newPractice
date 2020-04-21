5c134519cd08b7f1b5ec441d90f77544f111a560
Eve Stockstill <evestockstillpdx@gmail.com>
2020-04-21 16:26:02 -0700

    chunk

--- src/exercises/chunk/index.js
+++ src/exercises/chunk/index.js
@@ -8,6 +8,42 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// create empty array to hold chunks called 'chunked'
+// / for each element in the unchunked array
+// / retrieve the las element in the 'chunked
+// / IF last element does not exist, or if its length is equal to chunk size
+// / push a new chunk into 'chunked' with the current element
+// / else add the current element into the chunk
+
+// solution with a forEach
+// const chunk = (array, size) => {
+//   let chunked = [];
+//   array.forEach((element) => {
+//     const last = chunked[chunked.length - 1];
+//     if(!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   });
+//   return chunked;
+// };
+
+//  create empty 'chunked array
+// / create 'index start at 0
+// / while index is less than array.length
+// / push a slice of length 'size from array into 'chunked
+// / add size to index
+
+// solution with slice
+const chunk = (array, size) => {
+  let chunked = [];
+  let index = 0;
+  while(index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+};
 
 module.exports = chunk;
