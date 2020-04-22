41dc51d1e66fbebb58e3a2887032b69836a6e618
Eve Stockstill <evestockstillpdx@gmail.com>
2020-04-22 14:08:41 -0700

    pyramid solution 2 recursive

--- src/exercises/pyramid/index.js
+++ src/exercises/pyramid/index.js
@@ -14,21 +14,38 @@
 //       ' ### '
 //       '#####'
 
-const pyramid = (n) => {
-  const midpoint = Math.floor((2 * n - 1) / 2);
+// const pyramid = (n) => {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
 
-  for(let row = 0; row < n; row++) {
-    let level = '';
+//   for(let row = 0; row < n; row++) {
+//     let level = '';
 
-    for(let column = 0; column < 2 * n - 1; column++) {
-      if(midpoint - row <= column && midpoint + row >= column) {
-        level += '#';
-      } else {
-        level += ' ';
-      }
-    }
-    console.log(level);
+//     for(let column = 0; column < 2 * n - 1; column++) {
+//       if(midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// };
+
+const pyramid = (n, row = 0, level = '') => {
+  if(row === n) {
+    return;
   }
+  if(level.length === 2 * n - 1) {
+    return pyramid(n, row + 1);
+  }
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if(midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
 };
 
 module.exports = pyramid;
