26c1a407d242c59265c59b75fe97bfb717692830
Eve Stockstill <evestockstillpdx@gmail.com>
2020-04-23 13:41:09 -0700

    matrix hard problem

--- src/exercises/matrix/index.js
+++ src/exercises/matrix/index.js
@@ -15,6 +15,49 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+// create empty array of arrays called 'results'
+// creat a counter variable starting at 1
+// al long as (start column <= end column) AND (start row <= end row)
+// loop from start column to end column results[start_row[]i] assign counter variable increment counter increment start row
+// loop from start row to end row at results[i] [end_column] assign counter variable
+// increment counter
+// decrement end row...repeat for other two sides
 
+const matrix = (n) => {
+  const results = [];
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+  let counter = 1;
+  let startCol = 0;
+  let endCol = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+  while (startCol <= endCol && startRow <= endRow) {
+    //   top row
+    for (let i = startCol; i <= endCol; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+    // right column
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endCol] = counter;
+      counter++;
+    }
+    endCol--;
+    for (let i = endCol; i >= startCol; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startCol] = counter;
+      counter++;
+    }
+    startCol++;
+  }
+  return results;
+};
+// eslint-disable-next-line no-undef
 module.exports = matrix;
