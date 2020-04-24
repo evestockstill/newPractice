d28676a24c382af318f3afe5f4c5983692dc1217
Eve Stockstill <evestockstillpdx@gmail.com>
2020-04-23 21:40:17 -0700

    fib

--- src/exercises/fib/index.js
+++ src/exercises/fib/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable babel/no-invalid-this */
+/* eslint-disable no-undef */
 // --- Directions
 // Print out the n-th entry in the fibonacci series.
 // The fibonacci series is an ordering of numbers where
@@ -7,7 +9,47 @@
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
+//
 
-function fib(n) {}
+// slow fib
+// const fib = (n) => {
+//   if (n < 2) {
+//     return n;
+//   }
+//   return fib(n - 1) + fib(n - 2);
+// };
+
+// fast memoized fib
+
+const memoize = (fn) => {
+  const cache = {};
+  return (...args) => {
+    if (cache[args]) {
+      return cache[args];
+    }
+    const result = fn.apply(this, args);
+    return result;
+  };
+};
+
+const slowFib = (n) => {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+};
+const fib = memoize(slowFib);
+
+
+// const fib = _.memoize((n) => (n < 2 ? n : fib(n - 1) + fib(n - 2)));
+// const fib = (n) => {
+//   let result = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+//     result.push(a + b);
+//   }
+//   return result[n];
+// };
 
 module.exports = fib;
